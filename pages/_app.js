7c9a06ec79e7ed884afcd41c5aa24d02f2dae86e
Tad-z <david.akintade1000@gmail.com>
2023-08-04 11:42:00 +0100

    deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,53 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
-export default MyApp
+// export default MyApp
+import Router from "next/router";
+import React from "react";
+import { ColorRing } from "react-loader-spinner";
+
+export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div className="loader-container">
+        <div>
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+      </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
+}
